28576df4c38c2ee298f8483ee2521067f1245ade
a.rahimi <rahimi.a.2007@gmail.com>
2026-07-18 18:24:20 +0330

    add score, name, and color section

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const heart = document.getElementById("heart");
 const heartIcon = document.getElementById("heart-icon");
 
 heart.addEventListener("click", () => {
-  heartIcon.classList.toggle("text-primary");   
+  heartIcon.classList.toggle("text-primary");
   heartIcon.classList.toggle("fill-primary");
+});
+
+const colorName = document.getElementById("color-name");
+const colors = document.querySelectorAll("#colors button");
+
+colors.forEach(function (color) {
+  color.addEventListener("click", function () {
+    colors.forEach(function (color) {
+      color.classList.remove("border-2");
+      color.classList.remove("opacity-100");
+    });
+
+    color.classList.add("border-2");
+    color.classList.add("opacity-100");
+
+    colorName.textContent=color.dataset.name
+  });
 });
